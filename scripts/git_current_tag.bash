f6b40118a1ffc98f05c1035de9c001003e62c40f
tarmalonchik <alan.kochiev@dif.tech>
2025-10-09 02:05:16 +0400

    fxi

--- scripts/git_current_tag.bash
+++ scripts/git_current_tag.bash
@@ -2,6 +2,6 @@
 
 git fetch --tags
 
-latestVersion=$(git ls-remote --tags --sort=creatordate | grep -o 'v[0-9]*.[0-9]*.[0-9]*$' | tac | head -1)
+latestVersion=$(git ls-remote --tags --sort=creatordate | grep -o 'v[0-9]*.[0-9]*.[0-9]*$' | tail -r | head -1)
 
 echo "$latestVersion" 2>&1
