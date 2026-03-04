99297103645f3069142f823bc70aca6e154796cc
tarmalonchik <alnkochiev@gmail.com>
2026-03-04 17:00:06 +0400

    add gitsync to scripts

---
+++ scripts/gitsync.bash
@@ -0,0 +1,35 @@
+local REMOTE="origin"
+local BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+local FORCE=false
+
+if [[ -z "$BRANCH" ]]; then
+    echo "Error: Not a git repository."
+    return 1
+fi
+
+if [[ "$1" == "-f" ]]; then
+    FORCE=true
+fi
+
+echo "Sync branch [$BRANCH] with $REMOTE/$BRANCH..."
+
+if [ "$FORCE" = false ]; then
+    echo -n "Do you really want to sync current branch with origin? (y/n) "
+    read -k 1 REPLY
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo "Cancel operation."
+        return 1
+    fi
+fi
+
+echo "Running git fetch..."
+git fetch $REMOTE
+
+echo "Rollback to $REMOTE/$BRANCH..."
+git reset --hard $REMOTE/$BRANCH
+
+echo "Remove garbage (git clean)..."
+git clean -fd
+
+echo "Done!"
