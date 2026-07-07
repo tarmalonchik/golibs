cac9b4db26adf7d023e47ad2ad1ecfc5203ba5e8
tarmalonchik <alnkochiev@gmail.com>
2026-07-07 17:40:38 +0300

    fix

--- kafkawrapper/config.go
+++ kafkawrapper/config.go
@@ -15,12 +15,12 @@ var (
 )
 
 type Config struct {
-	KafkaPassword          string `env:"KAFKA_PASSWORD,required"`
-	KafkaUser              string `env:"KAFKA_USER,required"`
-	KafkaPort              string `env:"KAFKA_PORT,required"`
-	KafkaControllersCount  int    `env:"KAFKA_CONTROLLERS_COUNT,required"`
-	KafkaBrokerURLTemplate string `env:"KAFKA_BROKER_URL_TEMPLATE,required"`
-	KafkaReplicationFactor int    `env:"KAFKA_REPLICATION_FACTOR" envDefault:"3"`
-	KafkaEnableTLS         bool   `env:"KAFKA_ENABLE_TLS" envDefault:"true"`
-	KafkaPrefix            string `env:"KAFKA_PREFIX,required"`
+	KafkaPassword          string `env:"PASSWORD,required"`
+	KafkaUser              string `env:"USER,required"`
+	KafkaPort              string `env:"PORT,required"`
+	KafkaControllersCount  int    `env:"CONTROLLERS_COUNT,required"`
+	KafkaBrokerURLTemplate string `env:"BROKER_URL_TEMPLATE,required"`
+	KafkaReplicationFactor int    `env:"REPLICATION_FACTOR" envDefault:"3"`
+	KafkaEnableTLS         bool   `env:"ENABLE_TLS" envDefault:"true"`
+	KafkaPrefix            string `env:"PREFIX,required"`
 }
