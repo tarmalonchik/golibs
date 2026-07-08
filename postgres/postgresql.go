9d98e21c37711287269b5043974e8ac35cfbb404
tarmalonchik <alnkochiev@gmail.com>
2026-07-08 16:37:05 +0300

    sync once for postgres

--- postgres/postgresql.go
+++ postgres/postgresql.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
+	"sync"
 
 	"github.com/lib/pq"
 )
@@ -11,22 +12,27 @@ type Postgres struct {
 	db *sql.DB
 }
 
+var registerOnce sync.Once
+
 func New(cfg Config) (*Postgres, error) {
-	if cfg.GetPGDSN() != "" {
-		sql.Register("instrumented-postgres", pq.Driver{})
-		dbSQL, err := sql.Open("instrumented-postgres", cfg.GetPGDSN())
-		if err != nil {
-			return nil, fmt.Errorf("failed to connect database: %w", err)
-		}
-
-		dbSQL.SetMaxIdleConns(cfg.GetPGMaxIdleConns())
-		dbSQL.SetMaxOpenConns(cfg.GetPGMaxOpenConns())
-		dbSQL.SetConnMaxLifetime(cfg.GetPGTimeout())
-
-		return &Postgres{db: dbSQL}, nil
+	if cfg.GetPGDSN() == "" {
+		return nil, nil
 	}
 
-	return nil, nil
+	registerOnce.Do(func() {
+		sql.Register("instrumented-postgres", pq.Driver{})
+	})
+
+	dbSQL, err := sql.Open("instrumented-postgres", cfg.GetPGDSN())
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	dbSQL.SetMaxIdleConns(cfg.GetPGMaxIdleConns())
+	dbSQL.SetMaxOpenConns(cfg.GetPGMaxOpenConns())
+	dbSQL.SetConnMaxLifetime(cfg.GetPGTimeout())
+
+	return &Postgres{db: dbSQL}, nil
 }
 
 func (p *Postgres) GetDB() *sql.DB {
