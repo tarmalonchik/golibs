10a98f57efcbc3f6064d0dd909b766d78af4e79a
tarmalonchik <alnkochiev@gmail.com>
2026-07-10 18:28:29 +0300

    add redis default tls

--- redis/entities.go
+++ redis/entities.go
@@ -14,4 +14,5 @@ type Config struct {
 	RedisPort      string `env:"REDIS_PORT" envDefault:"6379"`
 	RedisPassword  string `env:"REDIS_PASSWORD" envDefault:""`
 	RedisKeyPrefix string `env:"REDIS_KEY_PREFIX,required"`
+	RedisEnableTLS bool   `env:"REDIS_ENABLE_TLS" envDefault:"true"`
 }
